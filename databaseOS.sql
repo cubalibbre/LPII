4b6709e97adda4abb651b359042b36359b3a855a
Jean Freitas Santos <135734216+jeanfreitas07@users.noreply.github.com>
2025-09-22 08:50:39 -0300

    Add files via upload

---
+++ databaseOS.sql
@@ -0,0 +1,14 @@
+ create database servicosDB;
+ 
+ create table orde_servico(
+	num_os int primary key,
+    descricao text,
+    tipo varchar(7)    
+ );
+ 
+ insert into ordem_servico values
+ (1,
+ "instalar impressora na sala dos professores",
+ "aberto");
+ 
+select * from ordem_servico
